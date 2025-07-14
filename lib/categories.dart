91403e756052640abc26df3a1ac68c9c4ec2875a
NizamUddin07 <nizzamuddin71@gmail.com>
2025-07-14 23:57:29 +0600

    category item added

--- lib/categories.dart
+++ lib/categories.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/main.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widget/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -16,12 +18,8 @@ class CategoriesScreen extends StatelessWidget {
             childAspectRatio: 3/2,
             crossAxisSpacing: 20, mainAxisSpacing: 20),
       children: [
-        Text("1", style: TextStyle(color: Colors.white),),
-        Text("2", style: TextStyle(color: Colors.white),),
-        Text("3", style: TextStyle(color: Colors.white),),
-        Text("4", style: TextStyle(color: Colors.white),),
-        Text("5", style: TextStyle(color: Colors.white),),
-        Text("6", style: TextStyle(color: Colors.white),),
+            for (final category in availableCategories)
+              CategoryGridItem(category: category)
         ],
       ),
     );
--- lib/widget/category_grid_item.dart
+++ lib/widget/category_grid_item.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
+import 'package:meals/model/category.dart';
 class CategoryGridItem extends StatelessWidget {
   const CategoryGridItem({super.key, required this.category});
 
@@ -7,6 +8,25 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+         colors: [
+            category.color.withOpacity(0.55),
+           category.color.withOpacity(0.9),
+         ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onBackground,
+        )
+      ),
+    );
   }
 }
